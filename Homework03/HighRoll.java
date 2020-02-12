4e60aab3fcaf408ec93315bbafe8f361c270afe5
smcgrogan <58006153+smcgrogan@users.noreply.github.com>
2020-02-12 10:16:56 -0800

    Final Update (probably)

--- Homework03/HighRoll.java
+++ Homework03/HighRoll.java
@@ -1,5 +1,5 @@
 /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- *  File name     :  HighRoll.java
+ *  File name     :  DiceSet.java
  *  Purpose       :  Provides a class describing a set of dice
  *  Author        :  Summer McGrogan
  *  Date          :  02.02.2020
@@ -10,6 +10,7 @@
    import java.io.IOException;
 
    public class HighRoll{
+
 
       public static void main( String args[] ) {
         int count = Integer.parseInt(args [0]);
@@ -25,37 +26,38 @@
             String inputLine = null;
             try {
                inputLine = input.readLine();
-               if( 0 == inputLine.length() ) {
+               if( '0' == inputLine.length() ) {
                   System.out.println("enter some text!:");
                   continue;
                }
                if( 'q' == inputLine.charAt(0) ) { //if this is 1, call dice.rollAll, if rollIndivid, as which one (add another read and add a parseInt to get the value)
                   break;
                }
-               if( 1 == inputLine.charAt(0)) {
+               if( '1' == inputLine.charAt(0)) {
                  dSet.roll();
                  System.out.println("You rolled a: " + dSet.toString());
                  System.out.println("Play Again!!");
                  System.out.print( "Option 1 : ROLL ALL THE DICE \nOption 2 : ROLL A SINGLE DIE \nOption 3 : CALCULATE THE SCORE FOR THIS SET \nOption 4 : SAVE THIS SCORE AS HIGH SCORE \nOption 5 : DISPLAY THE HIGH SCORE \nOption 6 : ENTER 'Q' TO QUIT THE PROGRAM\n");
                }
-               if( 2 == inputLine.charAt(0)) {
+               if( '2' == inputLine.charAt(0)) {
+                 System.out.print("Enter the die you want to roll again: ");
                  dSet.rollIndividual(Integer.parseInt(input.readLine()) - 1);
                  System.out.println("You rolled a: " + dSet.toString());
                  System.out.println("Play Again!!");
                  System.out.print( "Option 1 : ROLL ALL THE DICE \nOption 2 : ROLL A SINGLE DIE \nOption 3 : CALCULATE THE SCORE FOR THIS SET \nOption 4 : SAVE THIS SCORE AS HIGH SCORE \nOption 5 : DISPLAY THE HIGH SCORE \nOption 6 : ENTER 'Q' TO QUIT THE PROGRAM\n");
                }
-               if( 3 == inputLine.charAt(0)) {
+               if( '3' == inputLine.charAt(0)) {
                  System.out.println("Your scores sum is: " + dSet.sum());
                  System.out.println("Play Again!!");
                  System.out.print( "Option 1 : ROLL ALL THE DICE \nOption 2 : ROLL A SINGLE DIE \nOption 3 : CALCULATE THE SCORE FOR THIS SET \nOption 4 : SAVE THIS SCORE AS HIGH SCORE \nOption 5 : DISPLAY THE HIGH SCORE \nOption 6 : ENTER 'Q' TO QUIT THE PROGRAM\n");
                }
-               if( 4 == inputLine.charAt(0)) {
+               if( '4' == inputLine.charAt(0)) {
                  highScore = dSet.sum();
                  System.out.println("Your highscore is: " + highScore);
                  System.out.println("Play Again!!");
                  System.out.print( "Option 1 : ROLL ALL THE DICE \nOption 2 : ROLL A SINGLE DIE \nOption 3 : CALCULATE THE SCORE FOR THIS SET \nOption 4 : SAVE THIS SCORE AS HIGH SCORE \nOption 5 : DISPLAY THE HIGH SCORE \nOption 6 : ENTER 'Q' TO QUIT THE PROGRAM\n");
                }
-               if( 5 == inputLine.charAt(0)) {
+               if( '5' == inputLine.charAt(0)) {
                  System.out.print("Your highscore is: " + highScore);
                  System.out.println("Play Again!!");
                  System.out.print( "Option 1 : ROLL ALL THE DICE \nOption 2 : ROLL A SINGLE DIE \nOption 3 : CALCULATE THE SCORE FOR THIS SET \nOption 4 : SAVE THIS SCORE AS HIGH SCORE \nOption 5 : DISPLAY THE HIGH SCORE \nOption 6 : ENTER 'Q' TO QUIT THE PROGRAM\n");
