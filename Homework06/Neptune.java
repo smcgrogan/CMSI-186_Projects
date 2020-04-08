f2771b1153beac26eafebec129e82764be7ca4f8
smcgrogan <58006153+smcgrogan@users.noreply.github.com>
2020-04-07 23:32:25 -0700

    Updated Neptune --WARNING
    WARNING: I am very unsure if I understood the directions correctly. I gave it my best shot.

--- Homework06/Neptune.java
+++ Homework06/Neptune.java
@@ -19,27 +19,62 @@
 
  public class Neptune{
 
-    public static BrobInt speedL = new BrobInt("149896229");
-    public static BrobInt distanceH = new BrobInt("2200000000000");
+    public static BrobInt speedL = new BrobInt("149896229"); //149896229
+    public static BrobInt distanceH = new BrobInt("2200000000000"); //2200000000000
 
    public static void main(String[] args){
       BrobInt neptuneEsc = new BrobInt("23500");
+      BrobInt earthEsc = new BrobInt("11186");
       BrobInt speedE = new BrobInt("11186");
       BrobInt accelerat = new BrobInt(args[0]);
-      BrobInt tm = new BrobInt("0");
+      int tm = 0;
+      int seconds = 0;
+      int minutes = 0;
+      int hours = 0;
       System.out.println("inputted acceleration: " + accelerat + " meters per second");
-
-      while(accelerat.compareTo(BrobInt.ONE) > 0){
-        for (BrobInt i = BrobInt.ZERO; i.compareTo(speedL) < 0; i.add(accelerat)){
-           tm = tm.add(BrobInt.ONE);
+        if (accelerat.compareTo(BrobInt.TWO) < 0) {
+           System.out.println("NUMBER IS TOO SMALL. Enter a bigger number.");
+           System.exit(0);
+        }
+        for (BrobInt i = BrobInt.ZERO; i.compareTo(speedL) < 0; i = i.add(accelerat)){
+           tm += 1;
            speedE = speedE.add(accelerat);
+           System.out.println("Leaving for Neptune now...\nSpeed is: " + speedE);
         }
-        for (BrobInt j = BrobInt.ZERO; j.compareTo(neptuneEsc) < 0; j.add(accelerat)){
-           tm = tm.add(BrobInt.ONE);
+        for (BrobInt j = BrobInt.ZERO; j.compareTo(neptuneEsc) <= 0; j = j.add(accelerat)){
+           tm += 1;
            speedE = speedE.subtract(accelerat);
+           System.out.println("Approaching Neptune now...\nSpeed is: " + speedE);
         }
-        System.out.println("Reached Neptune at speed of: " + speedE + " in " + " seconds ");
-      }
+        System.out.println("Reached Neptune at speed of: " + speedE + " in " + tm + " seconds ");
+        for (BrobInt k = BrobInt.ZERO; k.compareTo(speedL) < 0; k = k.add(accelerat)){
+           tm += 1;
+           neptuneEsc = neptuneEsc.add(accelerat);
+           System.out.println("Returning from Neptune now.... \n Speed is " + neptuneEsc + " Approaching light speed! ");
+        }
+        for (BrobInt x = BrobInt.ZERO; x.compareTo(earthEsc) <= 0; x = x.add(accelerat)){
+           tm += 1;
+           neptuneEsc = neptuneEsc.subtract(accelerat);
+           System.out.println(" Slowing down to retun to earth at a speed of: " + neptuneEsc);
+        }
+        System.out.println("\n\nSafely returned to earth!\nAcceleration you choose: " + accelerat);
+
+        if (tm < 60){
+           seconds = tm;
+           System.out.println("TIME TAKEN: <" + hours + ":" + minutes + ":" + seconds + ">");
+        }
+        if ((tm > 60) & (tm < 3600)){
+           minutes = tm/60;
+           seconds = tm%60;
+           System.out.println("TIME TAKEN: <" + hours + ":" + minutes + ":" + seconds + ">");
+        }
+        if (tm > 3600){
+           hours = tm/3600;
+           minutes = tm%3600/60;
+           seconds = minutes%60;
+           System.out.println("TIME TAKEN: <" + hours + ":" + minutes + ":" + seconds + ">");
+        }
+      //}
 
 
 
